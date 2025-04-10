6d1ca00dc1ac57b4f1728d3e1a703d7c19154780
Rofiu255 <mujeeb.rofiu@gmail.com>
2025-04-10 15:14:38 +0000

    commit assignment three

---
+++ Assignments/Assignment3/Assignment3.js
@@ -0,0 +1,9 @@
+// 1. Create variables to store your name, age, and nationality. Combine them into a sentence that introduces you.
+var name = "Mujeeb Rofiu";
+var age = 25;
+var nationality = "Nigerian";
+
+
+var introduces = "I am ${name}";
+
+console.log(introduces);
